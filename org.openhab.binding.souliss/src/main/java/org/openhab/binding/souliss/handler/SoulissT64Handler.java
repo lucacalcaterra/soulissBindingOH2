ce4b5979a56de13f1a9d4a1d10ef521e54cb2923
lucarui <luca.remigio@hotmail.it>
2017-05-11 12:23:31 +0200

    REM - added all T6n handler classes. T6n base handler WIP

--- org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/handler/SoulissT64Handler.java
+++ org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/handler/SoulissT64Handler.java
@@ -3,17 +3,17 @@ package org.openhab.binding.souliss.handler;
 import org.eclipse.smarthome.core.thing.Thing;
 
 /**
- * The {@link SoulissT61Handler} is responsible for handling commands, which are
+ * The {@link SoulissT64Handler} is responsible for handling commands, which are
  * sent to one of the channels.
  *
  * @author Luca Remigio - Initial contribution
  */
-public class SoulissT61Handler extends SoulissT6nHandler {
+public class SoulissT64Handler extends SoulissT6nHandler {
 
     private float analogSetpointValue;
 
     // constructor
-    public SoulissT61Handler(Thing _thing) {
+    public SoulissT64Handler(Thing _thing) {
         super(_thing);
         // TODO Auto-generated constructor stub
     }

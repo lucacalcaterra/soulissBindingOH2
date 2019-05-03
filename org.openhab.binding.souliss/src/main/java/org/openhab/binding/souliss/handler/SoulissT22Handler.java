51afc5ce4cfbdcabd45bb6ec7c6edce0f0038a60
Tonino <fazioa@gmail.com>
2019-05-04 00:53:22 +0200

    T22 MOD

--- org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/handler/SoulissT22Handler.java
+++ org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/handler/SoulissT22Handler.java
@@ -12,6 +12,7 @@
  */
 package org.openhab.binding.souliss.handler;
 
+import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.library.types.StopMoveType;
 import org.eclipse.smarthome.core.library.types.StringType;
@@ -85,6 +86,8 @@ public class SoulissT22Handler extends SoulissGenericTypical implements typicalC
         if (_state != null) {
             if (_state instanceof UpDownType) {
                 this.updateState(SoulissBindingConstants.ROLLERSHUTTER_CHANNEL, (UpDownType) _state);
+                this.updateState(SoulissBindingConstants.ROLLERSHUTTER_STATE_NUMBER_CHANNEL_CHANNEL,
+                        (DecimalType) _state);
                 // this.updateThing(this.thing);
             } else if (_state instanceof StopMoveType) {
                 this.updateState(SoulissBindingConstants.ROLLERSHUTTER_CHANNEL, (State) _state);
